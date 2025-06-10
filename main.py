ad5bb2616848f4fb60d369fa0f86125b3c34eb98
MANOJALLU2 <139703306+MANOJALLU2@users.noreply.github.com>
2025-06-10 16:15:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -157,5 +157,6 @@ def api_product_transactions(id):
     return jsonify([x for x in txns if x['product_id'] == id])
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # default to 5000 if not set
+    app.run(host="0.0.0.0", port=port, debug=True)
